58764acc0b3546129d43681fbbf3283d4c565c63
Pive0528 <pive0528@gmail.com>
2024-10-24 13:12:40 +0900

    주문, 환불 기능 구현

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,16 @@
-# 개발 DB 생성
-
 DROP DATABASE IF EXISTS SpringBatch_app_10__dev;
 CREATE DATABASE SpringBatch_app_10__dev;
 USE SpringBatch_app_10__dev;
+
+DROP DATABASE IF EXISTS SpringBatch_app_10__test;
+CREATE DATABASE SpringBatch_app_10__test;
+USE SpringBatch_app_10__test;
+
+SHOW TABLES;
+
+SELECT * FROM `member`;
+SELECT * FROM `song`;
+SELECT * FROM `product`;
+SELECT * FROM `cart_item`;
+SELECT * FROM `cash_log`;
+SELECT * FROM `order_item`;
---
+++ src/main/java/com/koreait/exam/springbatch_app_10/app/order/repository/OrderItemRepository.java
@@ -0,0 +1,5 @@
+package com.koreait.exam.springbatch_app_10.app.order.repository;
+import com.koreait.exam.springbatch_app_10.app.order.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
